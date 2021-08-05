322241f1b2945662694f654b444eb22d30441429
mars-area <ilhammargatama@gmail.com>
2021-08-05 20:14:39 +0700

    tweak window

--- src/component/window.jsx
+++ src/component/window.jsx
@@ -12,22 +12,21 @@ class Window extends React.Component {
                             <span className="dot__3"></span>
                         </div>
                     <div className="window__body">
-                        <p>
-                            <TypeWriterEffect
-                                textStyle={{
-                                fontFamily: 'Inconsolata',
-                                fontSize: '1rem'
-                                }}
-                                startDelay={2000}
-                                cursorColor="#3F3D56"
-                                multiText={[
-                                'Hey there, This is me. I do Design, and Develop Websites.',
-                                'Have something great in mind? Feel free to contact me. I will help you to make it happen.'
-                                ]}
-                                multiTextDelay={1000}
-                                typeSpeed={30}
-                            />
-                        </p>
+                        <TypeWriterEffect
+                            textStyle={{
+                            fontFamily: 'Inconsolata',
+                            fontSize: '1rem'
+                            }}
+                            startDelay={2000}
+                            cursorColor="#3F3D56"
+                            multiText={[
+                            'Hey there, This is me. I do Design, and Develop Websites.',
+                            'Have something great in mind? Feel free to contact me. I will help you to make it happen.'
+                            ]}
+                            multiTextDelay={1000}
+                            typeSpeed={30}
+                        />
+                        
                     </div>
                 </div>
         );
