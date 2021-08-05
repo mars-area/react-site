65d1325a8a865a7d68b905a2721276c0d06138c9
mars-area <ilhammargatama@gmail.com>
2021-08-05 20:04:18 +0700

    remove react reveal

--- src/component/window.jsx
+++ src/component/window.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import './window.css';
-import Bounce from 'react-reveal/Bounce';
-import Slide from 'react-reveal/Slide';
 import TypeWriterEffect from 'react-typewriter-effect';
 
 class Window extends React.Component {
     render() {
         return(
-            <Bounce left>
                 <div className="window">
-                    <Slide left>
                         <div className="window__header">
                             <span className="dot__1"></span>
                             <span className="dot__2"></span>
                             <span className="dot__3"></span>
                         </div>
-                    </Slide>
                     <div className="window__body">
                         <p>
                             <TypeWriterEffect
@@ -35,7 +30,6 @@ class Window extends React.Component {
                         </p>
                     </div>
                 </div>
-            </Bounce>
         );
     }
 }
