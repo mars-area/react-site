e3f2957b9ae0c1a0bbb132aa55c3fa0901fcc624
mars-area <ilhammargatama@gmail.com>
2021-08-09 21:13:32 +0700

    create project section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Navi from './component/navi';
 import Header from './component/header';
+import Project from './component/project';
 
 function App() {
   return (
     <div className="App">
       <Navi/>
       <Header/>
+      <main>
+        <Project />
+      </main>
     </div>
   );
 }
---
+++ src/component/project.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// create react component for poject section
+export default class Project extends React.Component {
+    render() {
+        return (
+            <div className="project">
+                <h1>Project</h1>
+                <div className="project__wrapper">
+                    This is a project page.
+                </div>
+            </div>
+        );
+    }
+}
